176a3a0c79f31b99e03709542475253d5264b4e0
Alireza Kasir <alirezakasir@yahoo.com>
2022-03-07 17:48:30 +0330

    add carousel component to home section

--- src/main.js
+++ src/main.js
@@ -10,5 +10,8 @@ import "bootstrap/js/dist/collapse";
 // modal bootstrap
 import "bootstrap/js/dist/modal";
 
+// carousel bootstrap
+import "bootstrap/js/dist/carousel";
+
 // history items bootstrap
 import "./scripts/history-search";
