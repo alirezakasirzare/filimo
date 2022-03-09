22aafb37c1dfe2d63da3386c9a1dbeeaab5c35a5
Alireza Kasir <alirezakasir@yahoo.com>
2022-03-09 19:04:35 +0330

    responsive the silder of comment section

--- src/scripts/silders.js
+++ src/scripts/silders.js
@@ -58,24 +58,18 @@ tns({
 tns({
   container: "#comment-section-slider",
   controlsContainer: "#comment-controls",
-  items: 3,
+  items: 1,
   slideBy: "1",
   mouseDrag: true,
   nav: false,
   loop: false,
   gutter: 10,
   responsive: {
-    // 572: {
-    //   items: 3,
-    // },
-    // 768: {
-    //   items: 4,
-    // },
-    // 992: {
-    //   items: 5,
-    // },
-    // 1200: {
-    //   items: 6,
-    // },
+    572: {
+      items: 2,
+    },
+    992: {
+      items: 3,
+    },
   },
 });
