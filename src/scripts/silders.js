dbb23872a0214af97069b44810355ce1a71cd9ca
Alireza Kasir <alirezakasir@yahoo.com>
2022-03-09 11:30:27 +0330

    add arow controls structure to free section conrainer

--- src/scripts/silders.js
+++ src/scripts/silders.js
@@ -4,12 +4,12 @@ import "tiny-slider/dist/tiny-slider.css";
 var slider = tns({
   container: ".my-slider",
   items: 2,
-  slideBy: "page",
+  slideBy: "1",
   mouseDrag: true,
-  controls: false,
   nav: false,
   loop: false,
   gutter: 10,
+  controlsContainer: ".free-controls",
   responsive: {
     572: {
       items: 3,
