6a1a2647f3d450b61e4aac3259d7de4b92284a99
Alireza Kasir <alirezakasir@yahoo.com>
2022-03-09 13:00:01 +0330

    copy free section and naming that to children section

--- src/scripts/silders.js
+++ src/scripts/silders.js
@@ -27,3 +27,29 @@ tns({
     },
   },
 });
+
+// create children secrion silder
+tns({
+  container: "#children-section-slider",
+  controlsContainer: "#children-controls",
+  items: 2,
+  slideBy: "1",
+  mouseDrag: true,
+  nav: false,
+  loop: false,
+  gutter: 10,
+  responsive: {
+    572: {
+      items: 3,
+    },
+    768: {
+      items: 4,
+    },
+    992: {
+      items: 5,
+    },
+    1200: {
+      items: 6,
+    },
+  },
+});
