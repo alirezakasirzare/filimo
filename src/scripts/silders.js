a2bf31d95dde4410f58173affe759db98e788130
Alireza Kasir <alirezakasir@yahoo.com>
2022-03-09 10:20:47 +0330

    responsive free slider

--- src/scripts/silders.js
+++ src/scripts/silders.js
@@ -3,11 +3,25 @@ import "tiny-slider/dist/tiny-slider.css";
 
 var slider = tns({
   container: ".my-slider",
-  items: 5,
+  items: 2,
   slideBy: "page",
   mouseDrag: true,
   controls: false,
   nav: false,
   loop: false,
   gutter: 10,
+  responsive: {
+    572: {
+      items: 3,
+    },
+    768: {
+      items: 4,
+    },
+    992: {
+      items: 5,
+    },
+    1200: {
+      items: 6,
+    },
+  },
 });
