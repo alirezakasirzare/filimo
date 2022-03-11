0b705e55527c61fe69418780baf319ebf104bfd1
Alireza Kasir <alirezakasir@yahoo.com>
2022-03-11 15:59:02 +0330

    refactor postcss config file

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,17 @@
+// autoprefixer plugin
 const autoprefixer = require("autoprefixer");
+// purgecss plugin
 const purgecss = require("@fullhuman/postcss-purgecss");
+// rtlcss plugin
 const rtlcss = require("postcss-rtlcss");
 
 module.exports = {
+  // add plugins to postcss
   plugins: [
-    // autoprefixer,
-    // purgecss({
-    //   content: ["./src/**/*.html"],
-    // }),
+    autoprefixer,
+    purgecss({
+      content: ["./src/**/*.html"],
+    }),
     rtlcss,
   ],
 };
